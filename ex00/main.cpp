1a8b707e9791ad6a18d00eb16cf5fcf5397137ca
Cassy <119490174+cassy25@users.noreply.github.com>
2024-01-31 16:38:00 +0100

    Create main.cpp

---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+
+int main()
+{
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    const WrongAnimal* meta2 = new WrongAnimal();
+    const WrongAnimal* k = new WrongCat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound();
+    j->makeSound();
+    meta->makeSound();
+    k->makeSound();
+    meta2->makeSound();
+
+    delete meta;
+    delete meta2;
+    delete k;
+    delete j;
+    delete i;
+    return 0;
+}
